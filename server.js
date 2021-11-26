6b7b30c108c142ddff358d9854fd459cdf4a858c
petudeveloper <davidalvarezmazzo@gmail.com>
2021-11-25 22:29:29 -0500

    Add websockets.

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+
 const app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 const PORT = 3000;
 const dbUrl = 'mongodb+srv://petudeveloper:petujesusmazo@realtimechat.kkivb.mongodb.net/chatapp?retryWrites=true&w=majority';
 
 const Message = mongoose.model('Message',{ name : String, message : String})
+io.on("connection", () =>{
+  console.log("a user is connected")
+ })
 
 mongoose.connect(dbUrl , (err) => { 
   console.log('mongodb connected',err);
@@ -27,9 +33,11 @@ app.post('/messages', (req, res) => {
   message.save((err) =>{
     if(err)
       sendStatus(500);
+    io.emit('message', req.body);
     res.sendStatus(200);
   })
 })
-const server = app.listen(PORT, () => {
+
+const server = http.listen(PORT, () => {
   console.log(`Server is runnig on port ${PORT}`);
 });
