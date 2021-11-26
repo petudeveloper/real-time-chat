af1ba8ff822523d335b834c4a4bdcfac9cc3c386
petudeveloper <davidalvarezmazzo@gmail.com>
2021-11-25 22:54:09 -0500

    Use dontenv to manage the MongoDB url info. Update MongoDB user access info.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv')
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+dotenv.config()
 
 const app = express();
 var http = require("http").createServer(app);
@@ -9,7 +11,7 @@ var io = require("socket.io")(http);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 const PORT = 3000;
-const dbUrl = 'mongodb+srv://petudeveloper:petujesusmazo@realtimechat.kkivb.mongodb.net/chatapp?retryWrites=true&w=majority';
+const dbUrl = process.env.MONGODB_DB_URL;
 
 const Message = mongoose.model('Message',{ name : String, message : String})
 io.on("connection", () =>{
